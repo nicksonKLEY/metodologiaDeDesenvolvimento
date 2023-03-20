d2f2cd96dc1cbf64eaf10604404d614045b7e467
Angeluz20 <estevao.silva015@gmail.com>
2023-03-20 15:29:14 -0400

    developing the extended NavMaster of the Sidebar component

--- doc_board/src/components/NavMaster/index.tsx
+++ doc_board/src/components/NavMaster/index.tsx
@@ -1,28 +1,34 @@
 import Sidbar from '../Sidebar'
 import { ColorIcon, TitleLink, IconView, ViewLink } from './style'
 import { AiOutlineUser, AiOutlineSetting } from 'react-icons/ai'
+import React from 'react'
 
 export default function NavMaster() {
+  // const red = 'color-green-100 text-black'
+  // const normaLink = 'color-blue-100 text-white'
+
   return (
-    <Sidbar
-      itemNavigate1={
-        <ViewLink>
-          {' '}
-          <IconView>
-            <AiOutlineUser color={ColorIcon} size={25} />
-          </IconView>
-          <TitleLink to="/registerEmployee">Usuário</TitleLink>
-        </ViewLink>
-      }
-      itemNavigate2={
-        <ViewLink>
-          {' '}
-          <IconView>
-            <AiOutlineSetting color={ColorIcon} size={25} />
-          </IconView>
-          <TitleLink to="/registerteste">Config</TitleLink>
-        </ViewLink>
-      }
-    />
+    <React.Fragment>
+      <Sidbar
+        itemNavigate1={
+          <ViewLink>
+            {' '}
+            <IconView>
+              <AiOutlineUser color={ColorIcon} size={25} />
+            </IconView>
+            <TitleLink to="/registerEmployee">Usuário</TitleLink>
+          </ViewLink>
+        }
+        itemNavigate2={
+          <ViewLink>
+            {' '}
+            <IconView>
+              <AiOutlineSetting color={ColorIcon} size={25} />
+            </IconView>
+            <TitleLink to="/ConfigDashboard">Config</TitleLink>
+          </ViewLink>
+        }
+      />
+    </React.Fragment>
   )
 }
--- doc_board/src/components/NavMaster/style.tsx
+++ doc_board/src/components/NavMaster/style.tsx
@@ -1,24 +1,42 @@
 import styled, { keyframes } from 'styled-components'
 import { fadeInRight } from 'react-animations'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const FadeInRight = keyframes`${fadeInRight}`
 export const ColorIcon = '#5ef7a0'
+export const ColorActive = '#fff'
 
 export const ViewLink = styled.div`
   align-items: center;
   justify-content: space-around;
   background-color: #32385a;
   animation: 0.7s ${FadeInRight};
-  width: 90%;
+  width: 100%;
   height: 40px;
   display: flex;
   border-end-start-radius: 10px;
   border-start-start-radius: 10px;
   flex-direction: row;
+  margin-left: 10px;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 30px;
+    margin-left: 10px;
+  }
 `
-export const TitleLink = styled(Link)`
-  text-decoration: none;
+export const TitleLink = styled(NavLink)`
+  text-decoration: ${(props) => {
+    console.log(props.style)
+    return props.style
+      ? (isActive) => (isActive ? 'underline' : 'none')
+      : 'none'
+  }};
+  &:hover {
+    text-decoration: underline;
+  }
+  &[aria-current] {
+    color: #e5f9ee;
+  }
   color: #5ef7a0;
   text-align: center;
   align-items: center;
@@ -27,6 +45,9 @@ export const TitleLink = styled(Link)`
   width: 80%;
   display: flex;
   font-size: 20px;
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
 `
 export const IconView = styled.span`
   width: 30%;
