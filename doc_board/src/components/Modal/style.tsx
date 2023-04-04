08ce625ea90d329b0e68641fa47d08401b078cc2
Angeluz20 <estevao.silva015@gmail.com>
2023-04-04 12:43:03 -0400

    mod 90% complete

--- doc_board/src/components/Modal/style.tsx
+++ doc_board/src/components/Modal/style.tsx
@@ -1,7 +1,5 @@
-import styled, { keyframes } from 'styled-components'
-import { fadeInUp } from 'react-animations'
+import styled from 'styled-components'
 
-const FadeInRight = keyframes`${fadeInUp}`
 export const ContainerModal = styled.div`
   width: 100%;
   height: 100%;
@@ -9,16 +7,18 @@ export const ContainerModal = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #66698487;
-  opacity: 1;
   position: absolute;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
 `
 export const ViewModal = styled.div`
-  animation: 0.2s ${FadeInRight};
-  width: 450px;
-  height: 500px;
+  width: 45rem;
+  height: 50rem;
   background-color: #e6e7e8;
-  border-radius: 10px;
-  box-shadow: 0px 0px 20px #2225442e;
+  border-radius: 1rem;
+  box-shadow: 0rem 0rem 2rem #2225442e;
   align-items: center;
   justify-content: center;
   display: flex;
@@ -26,36 +26,36 @@ export const ViewModal = styled.div`
 `
 export const HeaderTitle = styled.header`
   width: 100%;
-  height: 80px;
+  height: 8rem;
   display: flex;
   justify-content: center;
   align-items: center;
 `
 export const TitleModal = styled.h1`
-  font-size: 24px;
+  font-size: 2.4rem;
   font-weight: bold;
   color: #2b2f44;
-  margin-top: 30px;
+  margin-top: 3rem;
 `
 export const ViewBtnExit = styled.div`
   width: 100%;
-  height: 30px;
+  height: 3rem;
   display: flex;
   align-items: flex-end;
   justify-content: flex-end;
 `
 export const BtnExit = styled.button`
-  width: 30px;
-  height: 30px;
-  border-radius: 40px;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 4rem;
   background-color: #fff;
   justify-content: center;
   font-weight: bold;
   text-align: center;
-  font-size: 20px;
+  font-size: 2rem;
   cursor: pointer;
-  margin-left: 30px;
-  border-width: 4px;
+  margin-left: 3rem;
+  border-width: 0.4rem;
   border-style: solid;
   border-color: #000;
   position: absolute;
@@ -71,15 +71,15 @@ export const ContainerForm = styled.form`
 `
 export const BtnCreateNewUser = styled.button`
   background-color: #40cd7e;
-  border-width: 2px;
+  border-width: 0.2rem;
   border-style: solid;
   border-color: #000;
-  border-radius: 30px;
+  border-radius: 3rem;
   font-weight: bold;
   outline: none;
-  height: 40px;
-  width: 140px;
-  font-size: 20px;
+  height: 4rem;
+  width: 14rem;
+  font-size: 2rem;
   color: #2b2f44;
   cursor: pointer;
 `
