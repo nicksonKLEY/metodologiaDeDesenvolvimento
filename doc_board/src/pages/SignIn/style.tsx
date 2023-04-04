fc5171fe34f9fb8935526f96e75ac544c3714281
Angeluz20 <estevao.silva015@gmail.com>
2023-04-04 12:46:35 -0400

    styling the LOGIN page

--- doc_board/src/pages/SignIn/style.tsx
+++ doc_board/src/pages/SignIn/style.tsx
@@ -11,57 +11,57 @@ export const Container = styled.div`
 `
 
 export const ViewSingIn = styled.form`
-  height: 450px;
-  width: 330px;
+  height: 45rem;
+  width: 33rem;
   background-color: #1f2442;
-  border-radius: 20px;
+  border-radius: 2rem;
   align-items: center;
   display: flex;
   justify-content: center;
   flex-direction: column;
   position: fixed;
-  border: 4px solid;
+  border: 0.4rem solid;
   border-color: #2fe57e;
 `
 export const ViewInput = styled.div`
   width: 80%;
   background-color: #32385a;
-  box-shadow: 0px 12px 10px #1e2340;
+  box-shadow: 0rem 1.2rem 1rem #1e2340;
   display: flex;
   flex-direction: row;
-  margin: 10px;
-  padding: 10px;
+  margin: 1rem;
+  padding: 1rem;
   align-items: center;
   justify-content: center;
-  border-radius: 7px;
+  border-radius: 0.7rem;
 `
 export const Input = styled.input`
-  width: 200px;
+  width: 20rem;
   background-color: #32385a;
-  margin-left: 5px;
+  margin-left: 0.5rem;
   color: #2fe57e;
   border: none;
   outline: none;
   font-weight: bold;
-  letter-spacing: 2px;
+  letter-spacing: 0.2rem;
   &::placeholder {
     color: #2fe57e;
   }
 `
 export const BtnAcess = styled.button`
-  width: 250px;
-  margin-top: 30px;
+  width: 25rem;
+  margin-top: 3rem;
   background-color: #2fe57e;
   border: none;
-  border-radius: 20px;
+  border-radius: 2rem;
   transition: all 0.5s;
   font-weight: bold;
-  letter-spacing: 2px;
-  font-size: 15px;
+  letter-spacing: 0.2rem;
+  font-size: 1.5rem;
   text-decoration: none;
   color: #212121;
   justify-content: center;
-  padding: 10px;
+  padding: 1rem;
   display: flex;
   cursor: pointer;
   &:hover {
@@ -70,8 +70,8 @@ export const BtnAcess = styled.button`
   }
 `
 export const Logo = styled.img`
-  width: 120px;
-  height: 90px;
-  margin-bottom: 30px;
+  width: 12rem;
+  height: 9rem;
+  margin-bottom: 3rem;
 `
 export const ColorIcon = '#5ef7a0'
