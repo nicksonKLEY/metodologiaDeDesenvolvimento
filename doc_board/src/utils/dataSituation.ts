d08b2a854c7ad825eda614fdf47a877e3692c1ed
marlisonmourao <bentesmourao@gmail.com>
2023-04-10 21:07:39 -0400

    fake user data

---
+++ doc_board/src/utils/dataSituation.ts
@@ -0,0 +1,29 @@
+export const dataSituation = [
+  {
+    name: 'Raimundo Nonato',
+    bank: 'Daycoval',
+    status: 'Aguardando digitação',
+    situation: 'waiting',
+  },
+
+  {
+    name: 'Maria do Perpetuo',
+    bank: 'Pan',
+    status: 'Documento pendente',
+    situation: 'pending',
+  },
+
+  {
+    name: 'Maria do Perpetuo',
+    bank: 'Safra',
+    status: 'Proposta Digitada',
+    situation: 'typing',
+  },
+
+  {
+    name: 'Terezinha Bentes',
+    bank: 'BMC',
+    status: 'Proposta Recusada',
+    situation: 'refused',
+  },
+] as const
