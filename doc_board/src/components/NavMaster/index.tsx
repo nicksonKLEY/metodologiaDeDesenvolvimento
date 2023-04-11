5cc9e3b6baa49a878312f23bce025d1ebba64f2e
Angeluz20 <estevao.silva015@gmail.com>
2023-04-11 00:15:13 -0400

    NavMaster: ok

--- doc_board/src/components/NavMaster/index.tsx
+++ doc_board/src/components/NavMaster/index.tsx
@@ -1,6 +1,17 @@
 import Sidbar from '../Sidebar'
-import { ColorIcon, TitleLink, IconView, ViewLink } from './style'
-import { AiOutlineUser } from 'react-icons/ai'
+import {
+  ViewItems,
+  TitleLink,
+  IconView,
+  ViewLink,
+  IconPerformance,
+  IconUser,
+  TxtLink,
+} from './style'
+
+// import { GrDocumentPerformance } from 'react-icons/gr'
+// eslint-disable-next-line no-unused-vars
+// import Graphic from '@mui/icons-material/Addchart'
 import React from 'react'
 
 export default function NavMaster() {
@@ -10,16 +21,27 @@ export default function NavMaster() {
   return (
     <React.Fragment>
       <Sidbar
-        itemNavigate1={
-          <ViewLink>
-            {' '}
-            <TitleLink to="/registerEmployee">
-              <IconView>
-                <AiOutlineUser color={ColorIcon} size={25} />
-              </IconView>
-              Usuário
-            </TitleLink>
-          </ViewLink>
+        itemNavigate={
+          <ViewItems>
+            <ViewLink>
+              {' '}
+              <TitleLink to="/registerEmployee">
+                <IconView>
+                  <IconUser />
+                </IconView>
+                <TxtLink>Usuário</TxtLink>
+              </TitleLink>
+            </ViewLink>
+            <ViewLink>
+              {' '}
+              <TitleLink to="/EmployeePerfomance">
+                <IconView>
+                  <IconPerformance />
+                </IconView>
+                <TxtLink>Desempenho</TxtLink>
+              </TitleLink>
+            </ViewLink>
+          </ViewItems>
         }
         tPage={'Master'}
       />
--- doc_board/src/components/NavMaster/style.tsx
+++ doc_board/src/components/NavMaster/style.tsx
@@ -1,16 +1,22 @@
-import styled, { keyframes } from 'styled-components'
-import { fadeInRight } from 'react-animations'
+import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
+import { TbChartInfographic } from 'react-icons/tb'
+import { AiOutlineUser } from 'react-icons/ai'
 
-const FadeInRight = keyframes`${fadeInRight}`
 export const ColorIcon = '#5ef7a0'
 export const ColorActive = '#fff'
 
+export const ViewItems = styled.div`
+  width: 100%;
+  height: 30rem;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
 export const ViewLink = styled.div`
   align-items: center;
   justify-content: space-around;
-  background-color: #32385a;
-  animation: 0.7s ${FadeInRight};
+  background-color: #474a6b;
   width: 100%;
   height: 4rem;
   display: flex;
@@ -18,10 +24,10 @@ export const ViewLink = styled.div`
   border-start-start-radius: 1rem;
   flex-direction: row;
   margin-left: 1rem;
+  margin-top: 0.5rem;
   @media (max-width: 768px) {
-    width: 100%;
-    height: 3rem;
-    margin-left: 1rem;
+    font-size: 3rem;
+    width: 90%;
   }
 `
 export const TitleLink = styled(NavLink)`
@@ -44,9 +50,11 @@ export const TitleLink = styled(NavLink)`
   cursor: pointer;
   width: 80%;
   display: flex;
-  font-size: 2rem;
+  font-size: 1.6rem;
   @media (max-width: 768px) {
-    font-size: 1.5rem;
+    font-size: 2rem;
+    justify-content: center;
+    width: 100%;
   }
 `
 export const IconView = styled.span`
@@ -55,4 +63,50 @@ export const IconView = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => {
+    console.log(props.style)
+    return props.style ? (isActive) => (isActive ? '#121212' : '#fff') : 'none'
+  }};
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+export const IconPerformance = styled(TbChartInfographic)`
+  font-size: 2.7rem;
+  color: ${(props) => {
+    console.log(props.style)
+    return props.style ? (isActive) => (isActive ? '#121212' : '#fff') : 'none'
+  }};
+  &:hover {
+    scale: 1.1;
+    transition: scale 300ms;
+    color: #97ebbb;
+  }
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
+`
+export const IconUser = styled(AiOutlineUser)`
+  font-size: 2.7rem;
+  color: ${(props) => {
+    console.log(props.style)
+    return props.style ? (isActive) => (isActive ? '#121212' : '#fff') : 'none'
+  }};
+  &:hover {
+    scale: 1.1;
+    transition: scale 300ms;
+    color: #97ebbb;
+  }
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
+`
+export const TxtLink = styled.p`
+  font-size: 1.4rem;
+  @media (max-width: 768px) {
+    display: none;
+  }
+  @media (max-width: 868px) {
+    font-size: 1.2rem;
+  }
 `
