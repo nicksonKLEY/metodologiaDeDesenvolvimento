387e7f63ec947288e52e31e1503def710895dae6
marlisonmourao <bentesmourao@gmail.com>
2023-04-27 12:04:27 -0400

    all

--- doc_board/src/App.tsx
+++ doc_board/src/App.tsx
@@ -1,12 +1,11 @@
-// import RoutesApp from './routes/Route'
+import RoutesApp from './routes/Route'
 import AuthProvider from './contexts/auth'
 import './App.css'
-import Seller from './pages/Seller'
 
 export function App() {
   return (
     <AuthProvider>
-      <Seller />
+      <RoutesApp />
     </AuthProvider>
   )
 }
